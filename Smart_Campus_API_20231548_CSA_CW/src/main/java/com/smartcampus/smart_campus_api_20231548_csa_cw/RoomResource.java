d40ab600a912fa226f2e0ff56d552c226d4e02db
Uthsara <savinduuthsara655@gmail.com>
2026-04-16 22:41:35 +0530

    Add Room endpoints and RoomNotEmptyException

---
+++ Smart_Campus_API_20231548_CSA_CW/src/main/java/com/smartcampus/smart_campus_api_20231548_csa_cw/RoomResource.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smart_campus_api_20231548_csa_cw;
+
+/**
+ *
+ * @author uthsarak
+ */
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Path("/rooms")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RoomResource {
+
+    // GET /api/v1/rooms — get all rooms
+    @GET
+    public Response getAllRooms() {
+        List<Room> roomList = new ArrayList<>(DataStore.getRooms().values());
+        return Response.ok(roomList).build();
+    }
+
+    // POST /api/v1/rooms — create a new room
+    @POST
+    public Response createRoom(Room room) {
+        if (room.getId() == null || room.getId().trim().isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\": \"Room ID i
