11d2a91f3d0e0a66ee912594c120c9b42cde4d2e
Uthsara <savinduuthsara655@gmail.com>
2026-04-17 11:59:02 +0530

    Add GlobalExceptionMapper and LoggingFilter

---
+++ Smart_Campus_API_20231548_CSA_CW/src/main/java/com/smartcampus/smart_campus_api_20231548_csa_cw/GlobalExceptionMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smart_campus_api_20231548_csa_cw;
+
+/**
+ *
+ * @author uthsarak
+ */
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    private static final Logger LOGGER = Logger.getLogger(GlobalExceptionMapper.class.getName());
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        LOGGER.log(Level.SEVERE, "Unexpected error occurred: ", exception);
+
+        Map<String, String> error = new LinkedHashMap<>();
+        error.put("error", "500 Internal Server Error");
+        error.put("message", "An unexpected error occurred. Please contact the administrator.");
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(error)
+                .build();
+    }
+}
---
+++ Smart_Campus_API_20231548_CSA_CW/src/main/java/com/smartcampus/smart_campus_api_20231548_csa_cw/LoggingFilter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.smart_campus_api_20231548_csa_cw;
+
+/**
+ *
+ * @author uthsarak
+ */
+public class LoggingFilter {
+    
+}
